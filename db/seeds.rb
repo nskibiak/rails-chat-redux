8bb58f48b25ac64d458ecbfcc1be8207a5c47da6
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-04 12:11:05 +0900

    finished initial seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO:
+
+# Destroy messages, users and channels (in that order)
+# Create some channels (general, paris, react for instance)
+# Create some users (needs email / password)
+# Create some messages in several channels.
+
+puts 'Clearing database...'
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts 'Creating users...'
+
+users = [
+  {email: "user1@ham.edu", password: "password"},
+  {email: "user2@ham.edu", password: "password"},
+  {email: "user3@ham.edu", password: "password"}
+]
+
+User.create(users)
+
+puts 'Creating channels...'
+
+channels = [
+  {name:"paris"},
+  {name:"react"},
+  {name:"general"}
+]
+
+Channel.create(channels)
+
+puts 'Creating messages...'
+
+message = {
+  user_id: 1,
+  channel_id: 4,
+  content: "I can't believe this worked!"
+}
+
+Message.create(message)
+
+puts 'Finished creating users, channels, and messages'
