1b08e1770588909845a45194705e5104398e7943
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-04 22:40:18 +0900

    created separate asset pipeline in imported react app for logo image

--- app/javascript/chat/components/app.jsx
+++ app/javascript/chat/components/app.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ChannelList from '../containers/channel_list';
 import MessageList from '../containers/message_list';
+import logo from '../assets/images/logo.svg';
 
 const App = () => {
   return (
     <div className="messaging-wrapper">
       <div className="logo-container">
-        <img className="messaging-logo" src="assets/images/logo.svg" alt="logo" />
+        <img className="messaging-logo" src={ logo } alt="logo" />
       </div>
       <ChannelList />
       <MessageList />
--- app/javascript/chat/index.jsx
+++ app/javascript/chat/index.jsx
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import PropTypes from 'prop-types'
-
-// const Hello = props => (
-//   <div>Hello {props.name}!</div>
-// )
-
-// Hello.defaultProps = {
-//   name: 'David'
-// }
-
-// Hello.propTypes = {
-//   name: PropTypes.string
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   ReactDOM.render(
-//     <Hello name="World" />,
-//     document.body.appendChild(document.createElement('div')),
-//   )
-// })
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
