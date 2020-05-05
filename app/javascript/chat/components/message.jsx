5ee9710c5974ea20934606625029ffc7d43e2fb8
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-05 11:03:23 +0900

    changed message component to display user_id instead of author to allow render

--- app/javascript/chat/components/message.jsx
+++ app/javascript/chat/components/message.jsx
@@ -13,12 +13,12 @@ function strToRGB(str) {
 }
 
 const Message = (props) => {
-  const { created_at, author, content } = props.message;
+  const { created_at, user_id, content } = props.message;
   const time = new Date(created_at).toLocaleTimeString();
   return (
     <div className="message-container">
       <i className="author">
-        <span style={{ color: strToRGB(author) }}>{author}</span>
+        <span style={{ color: strToRGB(user_id) }}>{user_id}</span>
         <small>{time}</small>
       </i>
       <p>{emojify(content)}</p>
