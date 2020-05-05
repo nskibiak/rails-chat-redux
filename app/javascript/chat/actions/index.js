57164c2e314c394a0a2a446a397501d29ee824bb
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-05 10:26:32 +0900

    updated fetch messages action url and credentials

--- app/javascript/chat/actions/index.js
+++ app/javascript/chat/actions/index.js
@@ -5,8 +5,8 @@ export const MESSAGE_POSTED = 'MESSAGE_POSTED';
 export const CHANNEL_SELECTED = 'CHANNEL_SELECTED';
 
 export function fetchMessages(channel) {
-  const url = `${BASE_URL}/${channel}/messages`;
-  const promise = fetch(url).then(r => r.json());
+  const url = `${BASE_URL}/channels/${channel}/messages`;
+  const promise = fetch(url, { credentials: "same-origin" }).then(r => r.json());
 
   return {
     type: FETCH_MESSAGES,
